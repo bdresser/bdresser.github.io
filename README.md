a360e7afae12410a96b48694d0cb3b514182e89b
bobby dresser <bobby.dresser@gmail.com>
2014-12-30 23:44:49 -0500

    hashpaths

--- README.md
+++
@@ -1,2 +0,0 @@
-bdresser.github.io
-==================
--- home.js
+++ home.js
@@ -1,24 +1,66 @@
-$( document ).ready(function(){
+$( document ).ready(function() {
 
-	//page.base('');
-	page('/', home);
-	page('/photo', photo);
-	page('/web', web);
-	page();
-
-	function photo(){
-		console.log("hello world");
-		//$('html').css("background", "none");
-		$('.indicator').text("on the photopage");
+	if(location.pathname === '/'){
+			showHome();
 	}
 
-	function home(){
-		$('.indicator').text("on the homepage");
+	function clearHome(){
+		console.log("clear home called");
+		$('.home-content').fadeOut(500);
 	}
 
-	function web(){
-
+	function clearPhoto(){
+		$('.photo-page').fadeOut(500);
 	}
+
+	function showHome() {
+		$('.home-content').fadeIn(600);
+	}
+
+	function showPhoto() {
+		$('.photo-page').fadeIn(400);
+	}
+
+	$('.photo-side').click(function(){
+		clearHome();
+	});
+
+	// $(".back-button").click(function(){
+	// 	showHome();
+	// })
+
+	Path.map("#/photo").enter(showPhoto).to(function(){
+ 		$('.home-content').hide();
+	}).exit(function(){
+		clearPhoto();
+		console.log(location.pathname);
+		if(location.pathname === '/'){
+			showHome();
+		}
+	});
+
+
+	// Path.map("#/something").enter(showHome()).to(function(){
+	// 	showHome();
+	// 	$('.photo-page').hide();
+	// });
+
+	
+	Path.listen();
+
+	// function photo(){
+	// 	console.log("hello world");
+	// 	//$('html').css("background", "none");
+	// 	$('.indicator').text("on the photopage");
+	// }
+
+	// function home(){
+	// 	$('.indicator').text("on the homepage");
+	// }
+
+	// function web(){
+
+	// }
 
 	$('.photo' )
 		.mouseenter(function(e) {
@@ -35,8 +77,5 @@ $( document ).ready(function(){
 		.mouseleave(function(e) {
 			$('.web-link').removeClass("white");
 		});
-
-
-	
 
 });
