dffc6fee5e01bc2eec200544d55670eba758f6ea
bobby dresser <bobby.dresser@gmail.com>
2015-05-31 17:54:01 -0400

    performance fix, arrow controls

--- scripts/home.js
+++ scripts/home.js
@@ -7,34 +7,45 @@ $( document ).ready(function() {
 
 		var body = $('.home-bg');
 
+		// change the background image and set timer
 	    function nextBackground() {
-
-	    	console.log(current);
 
 	    	body.fadeOut(200, function(){
 	    		changeImg(current);
 	    		current++;
 	    	});
-	    	
 
 	        timer = setTimeout(nextBackground, 5000);
 	    }
 
-	    // begin timer
+	    // begin slideshow
 	    timer = setTimeout(nextBackground(), 5000);
 
 	    // handle arrow press
 		$('body').keydown(function(e) {
 			if(e.keyCode == 37) { // left
 
-				console.log('left');
-		    	clearTimeout(timer);
-		    	backImg(current);
+				clearTimeout(timer);
+
+				console.log("current = ", current);
+				console.log("removing ", (current % 7));
+				console.log("adding ", (current-1)%7);
+		    	body.fadeOut(200, function(){
+		    		backImg(current);
+		    		current = ((current + 6) % 7);
+		    	});
+		    	console.log("after update, current = ", current);
 
 		    }
 
 		  	else if(e.keyCode == 39) { // right
 
+		  		clearTimeout(timer);
+
+		    	body.fadeOut(200, function(){
+		    		changeImg(current);
+		    		current++;
+		    	});
 		  	}	
 
 		});
@@ -46,9 +57,13 @@ $( document ).ready(function() {
 		var body = $('.home-bg');
 		var loader = $('.loader');
 		var backgrounds = ['bg1', 'bg2', 'bg3', 'bg4', 'bg5', 'bg6', 'bg7'];
+		var backgroundsLoaded = [0,0,0,0,0,0,0];
 
-        loader.removeClass(backgrounds[next % backgrounds.length]);
-	    loader.addClass(backgrounds[(next+1) % backgrounds.length]);
+		if (backgroundsLoaded[next+1] == 0){
+	        loader.removeClass(backgrounds[next % backgrounds.length]);
+		    loader.addClass(backgrounds[(next+1) % backgrounds.length]);
+		    backgroundsLoaded[next+1] = 1;
+		}
 
 		body.removeClass(backgrounds[current % backgrounds.length]);
 		body.addClass(backgrounds[(current+1) % backgrounds.length]);
@@ -57,10 +72,13 @@ $( document ).ready(function() {
 	}
 
     function backImg(current) {
+
     	var backgrounds = ['bg1', 'bg2', 'bg3', 'bg4', 'bg5', 'bg6', 'bg7'];
 		var body = $('.home-bg');
-    	body.removeClass(backgrounds[current]);
-    	body.addClass(backgrounds[current-- % backgrounds.length]);
+    	body.removeClass(backgrounds[current % backgrounds.length]);
+    	body.addClass(backgrounds[((current+6)%7) % backgrounds.length]);
+
+    	body.fadeIn(1000);
     };
 
 
@@ -68,16 +86,13 @@ $( document ).ready(function() {
 
 		var current = Math.floor(Math.random() * 7);
 		var backgrounds = ['bg1', 'bg2', 'bg3', 'bg4', 'bg5', 'bg6', 'bg7'];
-		$('.home-bg').addClass(backgrounds[current]);
-		$('.loader').addClass(backgrounds[current+1]);
 		console.log(current);
 
 		imagesLoaded('.home-bg', function(){
 			$('.home-all').fadeIn(2000);
 		});
 
-		cycleBG(current);
-		
+		cycleBG(current);		
 	}
 
 });
