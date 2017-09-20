5138d67b793c2a885741231fbb7386f25686f1cb
Bobby Dresser <bobby.dresser@gmail.com>
2017-09-20 16:49:23 -0700

    fix lazyload

--- scripts/photo.js
+++ scripts/photo.js
@@ -1,17 +1,16 @@
 $( document ).ready(function() {
 
-	// load image 100px before it appears
-	$(".fader").unveil(150);
+	lazyload();
 
 	// Fade images in when they appear in window ;)
 	$(window).scroll( function(){
         $('.fader').each( function(i){
             var top_of_object = $(this).position().top;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            if( bottom_of_window > (top_of_object + 100)){
+            if( bottom_of_window > (top_of_object + 140)){
                 $(this).animate({'opacity':'1'},500);
             }
-        });     
+        });
     });
 
 	$(".to-top").click(function() {
